e36eb1a6921c23e9186dcace7b7b882f26a8cd40
VYR4L <fkz.zanatt@gmail.com>
2024-05-08 09:29:09 -0300

    Criando os primeiros hashMaps

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,10 +6,24 @@ public class Main {
         int int_user_input2 = 0;
         int int_user_input3 = 0;
         int int_user_input4 = 0;
+
         List<String> lista_transicoes = new ArrayList<>();
         List<Integer> lista_estados = new ArrayList<>();
         List<String> list_alfabeto_sigma = new ArrayList<>();
         list_alfabeto_sigma.add("$");
+
+        // Isso funciona como se fosse um dict de python {chave: valor}, nesse caso (chave, valor)
+        // Preferi trabalhar com String, String porque depois dá pra concatenar e não será necessário converter
+        HashMap<String, String> codifica_fita = new HashMap<>();
+        HashMap<String, String> codifica_direcao = new HashMap<>();
+        HashMap<String, String> codifica_estado = new HashMap<>();
+
+        codifica_fita.put("$", "0");
+        codifica_fita.put("0", "11");
+        codifica_fita.put("1", "111");
+
+        codifica_direcao.put("D", "1");
+        codifica_direcao.put("E", "11");
 
         Scanner sc = new Scanner(System.in);
 
