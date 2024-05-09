2a152e3fdd6fa32c34d7fc4f95fb7d594dc4577b
Radajaaj <1vitor.camargo@gmail.com>
2024-05-09 13:36:11 -0300

    Quase pronto o cpp

--- TesteUsandoCPP/teste.cpp
+++ TesteUsandoCPP/teste.cpp
@@ -18,7 +18,7 @@ string dec_para_unario(int decimal){ //Converte um numero int para seu equivalen
     return unario;
 }
 
-ifstream& JumpLinha(ifstream& arquivo, int linha){  //Leva o ponteiro do arquivo para uma linha específica do input
+ifstream& jumpLinha(ifstream& arquivo, int linha){  //Leva o ponteiro do arquivo para uma linha específica do input
     arquivo.clear();
     arquivo.seekg(0, ios::beg); //Voltamos ao começo do arquivo
 
@@ -29,6 +29,38 @@ ifstream& JumpLinha(ifstream& arquivo, int linha){  //Leva o ponteiro do arquivo
     return arquivo;
 }
 
+string getCode(ifstream& arquivo, string simbolo){                  //Recebe um símbolo, retorna sua representação unária.
+    string linha;
+    int contador = 1;   //Armazena a representação decimal do símbolo
+                        //É inicializado como 1 em vez de 0, pois o símbolo 1 é o $.
+    
+    //Primeiro, vamos checar se é um símbolo de entrada
+    jumpLinha(arquivo, 1);
+    getline(arquivo, linha);
+    for(int i = 0; i < linha.length(); i++){    //Iteramos por todos os caracteres da linha
+        if(linha[i] != ' '){
+            contador++;
+            if(linha[i] == simbolo[0]){         //Encontramos o caractere!
+                goto exitLoop;
+            }
+        }
+    }
+
+    //Depois, vamos checar se é um símbolo de fita
+    getline(arquivo, linha);
+    for(int i = 0; i < linha.length(); i++){    //Iteramos por todos os caracteres da linha
+        if(linha[i] != ' ' && linha[i] != '$'){
+            contador++;
+            if(linha[i] == simbolo[0]){         //Encontramos o caractere!
+                goto exitLoop;
+            }
+        }
+    }
+
+    exitLoop:
+    return dec_para_unario(contador);     //Convertemos para unário, e retornamos a representação do símbolo!
+
+}
 
 
 
@@ -118,9 +150,85 @@ int main(int argc, char* argv[]){ //Argc = n° de parametros + nome do arquivo .
 
     //Input Linha 6 - n° de transições da MT.
     //Pulamos pra linha 6:
-    JumpLinha(arquivo, 5); //LEMBRETE - Devemos pular para a linha 5, pois o getlina vai ler a linha seguinte.
+    jumpLinha(arquivo, 5); //LEMBRETE - Devemos pular para a linha 5, pois o getline vai ler a linha seguinte.
     getline(arquivo, linha);
-    cout << endl << "Linha 6 eh" << endl << linha << endl;
+    int nTransicoes = stoi(linha);
+    cout << endl << "Possuimos " << nTransicoes << " transicoes." << endl;
+
+
+    //Output 4: * t linhas que representarão cada uma das transições;
+    //          Formato EA + 0 + SL + 0 + PE + 0 + SE + 0 + Dir
+    //          Nota: a ordem dos caracteres: $ + Σ + Γ.
+    //          Apesar de $ pertencer a Γ, ele é representado pelo código "1".
+    //              Não está definido nas especificações do trabalho, mas isso fica implícito nos exemplos 1 e 2.
+
+    string aux; //String auxiliar
+    int pos;    //Vai armazenar a posição de determinados caracteres dentro de uma string
+
+    //Temos n Transições. Loop for para pegar todas elas usando jumpLinha() e getline(), a partir da linha 6:
+    for(i = 6; i < (nTransicoes + 6); i++){
+        jumpLinha(arquivo, i);
+        getline(arquivo, linha);
+        cout << endl << "Linha inicial: - " << linha << endl;
+        //Leitura do EA (Estado Atual)
+            //Os estados vão de 1 a 20, e a sua representação unária é equivalente ao seu número. Logo, só precisamos pegar o nome do estado e converter ele para unário.
+
+        // EA  -  Estado Anterior
+        pos = linha.find(" ");
+        aux = linha.substr(0, pos);     //Pegamos o EA e colocamos ele na substring aux
+        linha.erase(0, pos + 1);            //Apagamos o EA da string original (o +1 serve para apagar o espaço também)
+
+        cout << endl << "EA - " << aux << endl;
+        cout << "Linha - " << linha;
+        saida += dec_para_unario(stoi(aux));//Colocamos o código unário do EA na saida
+
+
+        saida += '0';                       //E um 0 para separar
+        // SL  -  Símbolo Lido
+        pos = linha.find(" ");
+        aux = linha.substr(0, pos);     //Pegamos o SL e colocamos ele na substring aux
+        linha.erase(0, pos + 1);            //Apagamos o SL da string original
+        if(aux == "$"){                 //Caso especial para garantir que $ seja representado como "1".
+            saida += '1';
+        }else{
+            saida += getCode(arquivo, aux); //Obtemos o código de SL, e inserimos na saída
+        }
+
+
+
+
+
+
+
+
+
+
+
+        saida += '0';
+        // PE  -  Próximo Estado
+        pos = linha.find(" ");
+        aux = linha.substr(0, pos);     //Pegamos o PE e colocamos ele na substring aux
+        linha.erase(0, pos + 1);            //Apagamos o PE da string original
+
+
+        saida += '0';
+        // SE  -  Símbolo Escrito
+        pos = linha.find(" ");
+        aux = linha.substr(0, pos);     //Pegamos o SE e colocamos ele na substring aux
+        linha.erase(0, pos + 1);            //Apagamos o SE da string original
+
+
+        saida += '0';
+        // Dir  -  Direção
+        //Na string linha só sobrou a direção
+
+
+
+
+        saida += "\n";
+    }
+
+        cout << endl << "SAIDA" << endl << saida;
 
 
 
