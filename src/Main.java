05081335ea5fb6d19c77c275af86e8242e1f2edc
VYR4L <fkz.zanatt@gmail.com>
2024-05-10 19:00:21 -0300

    Trabalho pronto, porém falta embelezar o código

--- src/Main.java
+++ src/Main.java
@@ -6,128 +6,129 @@ public class Main {
     public static void main(String[] args) {
         String concat = "1";
         String concat2 = "1";
+        String concat3 = "1";
 
-        int int_user_input = 0;
+        int numero_estados = 0;
         int int_user_input2 = 0;
         int int_user_input3 = 0;
         int int_user_input4 = 0;
         int tamanho_alfabetos = 0;
 
-        List<String> dois_alfabetos = new ArrayList<>();
         List<String> lista_transicoes = new ArrayList<>();
+        List<String> list_alfabeto_sigma = new ArrayList<>();
         List<String> list_alfabeto_gama = new ArrayList<>();
-        list_alfabeto_gama.add("$");
 
-        // Isso funciona como se fosse um dict de python {chave: valor}, nesse caso (chave, valor)
-        // Preferi trabalhar com String, String porque depois dá pra concatenar e não será necessário converter
-        HashMap<String, String> codifica_fita = new HashMap<>(); // Iterável
+        HashMap<String, String> codifica_fita = new HashMap<>();
         HashMap<String, String> codifica_simbolos_fita = new HashMap<>();
         HashMap<String, String> codifica_direcao = new HashMap<>();
-        HashMap<String, String> codifica_estado = new HashMap<>(); // Iterável
+        HashMap<String, String> codifica_estado = new HashMap<>();
+        HashMap<String, String> codifica_estados_finais = new HashMap<>();
 
         codifica_direcao.put("D", "1");
         codifica_direcao.put("E", "11");
 
-        // Lendo do arquivo
         File file = new File("C:\\Users\\fkzza\\Documents\\Trabalho_TC\\untitled\\src\\entrada.txt");
         try {
             Scanner sc = new Scanner(file);
 
-            // Número de Estados
             String user_input = sc.nextLine();
-            int_user_input = Integer.parseInt(user_input);
+            numero_estados = Integer.parseInt(user_input);
 
-            // chave valor (estado, codificação):
-            for (int i = 1; i <= int_user_input; i++) {
+            for (int i = 1; i <= numero_estados; i++) {
                 String string_i = String.valueOf(i);
                 codifica_estado.put(string_i, concat);
                 concat += "1";
             }
 
-            // Alfabeto Σ
             String user_input2 = sc.nextLine();
             String[] alfabeto_sigma = user_input2.split(" ");
-            List<String> list_alfabeto_sigma = Arrays.asList(alfabeto_sigma);
+            list_alfabeto_sigma.addAll(Arrays.asList(alfabeto_sigma));
 
-
-            // Alfabeto Γ
             String user_input3 = sc.nextLine();
             String[] alfabeto_gama = user_input3.split(" ");
-            List<String> list_alfabeto_gama_c = Arrays.asList(alfabeto_gama);
-            list_alfabeto_gama.addAll(list_alfabeto_gama_c);
+            list_alfabeto_gama.addAll(Arrays.asList(alfabeto_gama));
 
+            // Adicionando o símbolo $ no início do alfabeto Γ
+            if (list_alfabeto_gama.contains("$")) {
+                list_alfabeto_gama.remove("$");
+                list_alfabeto_gama.add(0, "$");
+            }
 
-            tamanho_alfabetos = list_alfabeto_gama.size() + list_alfabeto_gama_c.size();
-            dois_alfabetos.addAll(list_alfabeto_gama);
-            dois_alfabetos.addAll(list_alfabeto_sigma);
+            // Unindo os alfabetos Σ e Γ
+            list_alfabeto_sigma.addAll(list_alfabeto_gama);
 
-            for (int i = 0; i < tamanho_alfabetos; i++) {
-                codifica_fita.put(dois_alfabetos.get(i), concat2);
+            // Ordenando os símbolos de acordo com a regra
+            Collections.sort(list_alfabeto_sigma);
+
+            for (String symbol : list_alfabeto_sigma) {
+                codifica_fita.put(symbol, concat2);
                 concat2 += "1";
             }
 
-            // Estado inicial
             String user_input4 = sc.nextLine();
             int_user_input2 = Integer.parseInt(user_input4);
 
-
-            // Estado(s) final(is)
             String user_input5 = sc.nextLine();
-            int_user_input3 = Integer.parseInt(user_input5);
+            String[] estados_finais = user_input5.split(" ");
+            for (String estado : estados_finais) {
+                codifica_estados_finais.put(estado, concat);
+            }
 
-
-            // Número de transições
             String user_input6 = sc.nextLine();
             int num_transicoes = Integer.parseInt(user_input6);
 
-
-            // Saída:
-            System.out.println("Representação R<z>:");
-
-            // Estados
-            System.out.println("Estados:");
-            for (int i = 1; i <= int_user_input; i++) {
+            for (int i = 1; i <= numero_estados; i++) {
                 System.out.println(codifica_estado.get(String.valueOf(i)));
             }
 
-            // Símbolos de Σ e Γ
-            System.out.println("Símbolos de Σ e Γ:");
-            for (String simbolo : dois_alfabetos) {
-                if (!codifica_simbolos_fita.containsKey(simbolo)) {
-                    codifica_simbolos_fita.put(simbolo, concat2);
-                    concat2 += "1";
+            for (String symbol : list_alfabeto_sigma) {
+                if (!codifica_simbolos_fita.containsKey(symbol)) {
+                    codifica_simbolos_fita.put(symbol, concat3);
+                    concat3 += '1';
                 }
-                System.out.println(codifica_simbolos_fita.get(simbolo));
+                System.out.println(codifica_simbolos_fita.get(symbol));
             }
 
-            // Direções
-            System.out.println("Direções:");
             System.out.println(codifica_direcao.get("D"));
             System.out.println(codifica_direcao.get("E"));
 
-            System.out.println("Transições:");
             for (int i = 0; i < num_transicoes; i++) {
                 String transicao = sc.nextLine();
                 String[] partesTransicao = transicao.split(" ");
-
-                // Codifica cada parte da transição individualmente
                 String transicaoCodificada =
                         codifica_estado.get(partesTransicao[0]) + "0" + // Estado atual
-                        codifica_fita.get(partesTransicao[1]) + "0" +  // Símbolo lido
-                        codifica_estado.get(partesTransicao[2]) + "0" + // Próximo estado
-                        codifica_fita.get(partesTransicao[3]) + "0" +  // Símbolo escrito
-                        codifica_direcao.get(partesTransicao[4]); // Direção
+                                codifica_fita.get(partesTransicao[1]) + "0" +  // Símbolo lido
+                                codifica_estado.get(partesTransicao[2]) + "0" + // Próximo estado
+                                codifica_fita.get(partesTransicao[3]) + "0" +  // Símbolo escrito
+                                codifica_direcao.get(partesTransicao[4]); // Direção
 
                 lista_transicoes.add(transicaoCodificada.trim());
             }
-            System.out.println(lista_transicoes);
+            for (String transicao : lista_transicoes) {
+                System.out.println(transicao);
+            }
 
-            // Estados finais
-            System.out.println("Estados Finais:");
-            System.out.println(int_user_input3);
-
+            for (String estado : estados_finais) {
+                System.out.println(codifica_estado.get(estado));
+            }
+            
             // Representação R<M> final
-            System.out.println("Representação R<M> final");
+            StringBuilder finalRepresentation = new StringBuilder();
+            for (String estado : estados_finais) {
+                finalRepresentation.append(codifica_estado.get(estado));
+            }
+            finalRepresentation.append("00");
+
+            // Transições
+            for (String transicao : lista_transicoes) {
+                finalRepresentation.append(transicao).append("00");
+            }
+
+            if (finalRepresentation.length() >= 2) {
+                finalRepresentation.setLength(finalRepresentation.length() - 2);
+            }
+
+            System.out.println(finalRepresentation);
 
             sc.close(); // Fechando o scanner
 
